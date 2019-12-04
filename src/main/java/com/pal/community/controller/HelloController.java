f0f71d2a64338aa444612f49157b9d87f9add0f8
pal <925878482@qq.com>
2019-12-04 14:48:07 +0800

    init

---
+++ src/main/java/com/pal/community/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.pal.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    public String Hello(){
+        return "hello";
+    }
+}
