6587e32b229841741624f71cd50837bb6c267783
pal <925878482@qq.com>
2019-12-09 17:32:28 +0800

    profile

---
+++ src/main/java/com/pal/community/controller/ProfileController.java
@@ -0,0 +1,19 @@
+package com.pal.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+@Controller
+public class ProfileController {
+    @GetMapping("/profile/{action}")
+    public String profile(@PathVariable(name = "action") String action,
+                          Model model){
+        if ("questions".equals(action)) {
+            model.addAttribute("section","questions");
+            model.addAttribute("sectionName","我的提问");
+
+        }
+        return "profile";
+    }
+}
